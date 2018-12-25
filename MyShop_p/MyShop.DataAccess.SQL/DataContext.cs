5bc4e46bef08bd8e2055b80af4e42ee02abebd1d
petarbazant <petarbazant@gmail.com>
2018-12-25 21:08:51 +0100

    Installed EF, created DB PM> Enable-Migrations PM> Add-Migration Initial PM> Update-Database

---
+++ MyShop_p/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL
+{
+    class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        } 
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
