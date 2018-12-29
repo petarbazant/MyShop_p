fa55916686747f27e3f5cdf2af37c50a6e6381a3
petarbazant <petarbazant@gmail.com>
2018-12-29 22:13:53 +0100

    Basket model, update DB PM> Add-migration AddBasket PM>Update-Database

--- MyShop_p/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop_p/MyShop.DataAccess.SQL/DataContext.cs
@@ -11,12 +11,14 @@ namespace MyShop.DataAccess.SQL
     public class DataContext : DbContext
     {
         public DataContext()
-            :base("DefaultConnection")
+            : base("DefaultConnection")
         {
 
-        } 
+        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
