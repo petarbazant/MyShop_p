c346bb4d2acc62196521513aa3374b0bb4c34c95
petarbazant <petarbazant@gmail.com>
2018-12-30 11:25:50 +0100

    style

--- MyShop_p/MyShop.Core/ViewModels/BasketItemViewModel.cs
+++ MyShop_p/MyShop.Core/ViewModels/BasketItemViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace MyShop.Core.ViewModels
     {
         public string Id { get; set; }
         public int Quantity { get; set; }
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
