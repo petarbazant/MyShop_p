d3faacdcb0761c03732c36a64bf08c322b5a8c6f
Petar Bazant <petarbazant@gmail.com>
2019-01-21 21:51:08 +0100

    Basket Test Complete

---
+++ MyShop_p/MyShop.WebUI.Tests/Mocks/MockHttpContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockHttpContext : HttpContextBase
+    {
+        MockRequest request;
+        MockResponse response;
+        HttpCookieCollection cookies;
+
+        public MockHttpContext()
+        {
+            cookies = new HttpCookieCollection();
+            request = new MockRequest(cookies);
+            response = new MockResponse(cookies);
+        }
+
+
+        public override HttpRequestBase Request
+        {
+            get { return request; }
+        }
+
+        public override HttpResponseBase Response
+        {
+            get { return response; }
+        }
+
+    }
+
+    public class MockResponse : HttpResponseBase
+    {
+        private readonly HttpCookieCollection cookies;
+
+        public MockResponse(HttpCookieCollection cookies)
+        {
+            this.cookies = cookies;
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return cookies;
+            }
+        }
+    }
+
+    public class MockRequest : HttpRequestBase
+    {
+        private readonly HttpCookieCollection cookies;
+
+        public MockRequest(HttpCookieCollection cookies)
+        {
+            this.cookies = cookies;
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return cookies;
+            }
+        }
+    }
+
+}
